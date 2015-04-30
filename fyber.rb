05e6f661dcb61cac5fff385270910a21b1be9761
Miguel Grazziotin <miguelgraz@gmail.com>
2015-04-30 14:26:14 -0300

    inform and use the already given parameters

--- fyber.rb
+++ fyber.rb
@@ -5,8 +5,6 @@ class FyberCli < Sinatra::Base
   APIKey = 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
   PARAMS = {
     appid: 157,
-    apple_idfa: '2E7CE4B3-F68A-44D9-A923-F4E48D92B31E',
-    apple_idfa_tracking_enabled: false,
     device_id: '2b6f0cc904d137be2e1730235f5664094b83',
     format: 'json',
     ip: '109.235.143.113',
